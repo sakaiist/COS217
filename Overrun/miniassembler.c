3321758dfad9c1e792e4db3b23f9cdf3bfbaebf4
sakaiist <shealenjunk@gmail.com>
2025-05-24 14:26:40 -0700

    add Overrun

---
+++ Overrun/miniassembler.c
@@ -0,0 +1,73 @@
+/*--------------------------------------------------------------------*/
+/* miniassembler.c                                                    */
+/* Author: Bob Dondero, Donna Gabai                                   */
+/*--------------------------------------------------------------------*/
+
+#include "miniassembler.h"
+#include <assert.h>
+#include <stddef.h>
+
+/*--------------------------------------------------------------------*/
+/* Modify *puiDest in place,
+   setting uiNumBits starting at uiDestStartBit (where 0 indicates
+   the least significant bit) with bits taken from uiSrc,
+   starting at uiSrcStartBit.
+   uiSrcStartBit indicates the rightmost bit in the field.
+   setField sets the appropriate bits in *puiDest to 1.
+   setField never unsets any bits in *puiDest.                        */
+static void setField(unsigned int uiSrc, unsigned int uiSrcStartBit,
+                     unsigned int *puiDest, unsigned int uiDestStartBit,
+                     unsigned int uiNumBits)
+{
+   /* Your code here */
+
+}
+
+/*--------------------------------------------------------------------*/
+
+unsigned int MiniAssembler_mov(unsigned int uiReg, int iImmed)
+{
+   /* Your code here */
+
+}
+
+/*--------------------------------------------------------------------*/
+
+unsigned int MiniAssembler_adr(unsigned int uiReg, unsigned long ulAddr,
+   unsigned long ulAddrOfThisInstr)
+{
+   unsigned int uiInstr;
+   unsigned int uiDisp;
+
+   /* Base Instruction Code */
+   uiInstr = 0x10000000;
+
+   /* register to be inserted in instruction */
+   setField(uiReg, 0, &uiInstr, 0, 5);
+
+   /* displacement to be split into immlo and immhi and inserted */
+   uiDisp = (unsigned int)(ulAddr - ulAddrOfThisInstr);
+
+   setField(uiDisp, 0, &uiInstr, 29, 2);
+   setField(uiDisp, 2, &uiInstr, 5, 19);
+
+   return uiInstr;
+}
+
+/*--------------------------------------------------------------------*/
+
+unsigned int MiniAssembler_strb(unsigned int uiFromReg,
+   unsigned int uiToReg)
+{
+   /* Your code here */
+
+}
+
+/*--------------------------------------------------------------------*/
+
+unsigned int MiniAssembler_b(unsigned long ulAddr,
+   unsigned long ulAddrOfThisInstr)
+{
+   /* Your code here */
+
+}
